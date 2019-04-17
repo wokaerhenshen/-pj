90acc000f8bc1f1415cd5fd1c73b75e22f7f0f4b
wokaerhenshen <karlxu0410@gmail.com>
2019-04-16 23:42:05 -0700

    more change in jianshu

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {CSSTransition} from 'react-transition-group';
 import {actionCreators}  from './store';
@@ -16,36 +16,35 @@ import {HeaderWrapper,
         SearchInfoItem,
         SearchInfoList} from './style.js';
 
+class Header extends Component{
 
-const getListArea = (show) => {
-    if(show){
-        return (
-            <SearchInfo>
-            <SearchInfoTitle>
-                Hot Search
-                <SearchInfoSwitch>
-                    Change
-                </SearchInfoSwitch>
-            </SearchInfoTitle>
-            <SearchInfoList>
-                <SearchInfoItem>karl</SearchInfoItem>
-                <SearchInfoItem>karl</SearchInfoItem>
-                <SearchInfoItem>karl</SearchInfoItem>
-                <SearchInfoItem>karl</SearchInfoItem>
-                <SearchInfoItem>karl</SearchInfoItem>
-                <SearchInfoItem>karl</SearchInfoItem>
-            </SearchInfoList>
-            </SearchInfo>
-        )
-    }else {
-        return null;
+    getListArea(){
+        if(this.props.focused){
+            return (
+                <SearchInfo>
+                <SearchInfoTitle>
+                    Hot Search
+                    <SearchInfoSwitch>
+                        Change
+                    </SearchInfoSwitch>
+                </SearchInfoTitle>
+                <SearchInfoList>
+                {
+                    this.props.list.map((item) => {
+                        return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+                    })
+                }
+                </SearchInfoList>
+                </SearchInfo>
+            )
+        }else {
+            return null;
+        }
     }
-}
 
-
-const Header = (props) => {
-    return (
-        <HeaderWrapper>
+    render(){
+        return (
+            <HeaderWrapper>
             <Logo />
             <Nav>
                 <NavItem className='left active'>Main Page</NavItem>
@@ -56,17 +55,17 @@ const Header = (props) => {
                 </NavItem>
                 <SearchWrapper>
                     <CSSTransition
-                    in={props.focused}
+                    in={this.props.focused}
                     timeout={200}
                     classNames='slide'
                     >
-                    <NavSearch className = {props.focused ? 'focused' : ''}
-                            onFocus={props.handleInputFocus}
-                            onBlur={props.handleInputBlur}>
+                    <NavSearch className = {this.props.focused ? 'focused' : ''}
+                            onFocus={this.props.handleInputFocus}
+                            onBlur={this.props.handleInputBlur}>
                     </NavSearch>
                     </CSSTransition>
-                <span className = {props.focused ? 'focused iconfont' : 'iconfont'}>&#xe633;</span>
-                {getListArea(props.focused)}
+                <span className = {this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe633;</span>
+                {this.getListArea()}
                 </SearchWrapper>
             </Nav>
             <Addition>
@@ -77,20 +76,25 @@ const Header = (props) => {
                 <Button className='reg'>Register</Button> 
             </Addition>    
         </HeaderWrapper>
-    )
+        )
+
+    }
 }
+
 
 
 const mapStateToProps = (state) => {
     return {
         //focused : state.get('header').get('focused')
-        focused : state.getIn(['header','focused'])
+        focused : state.getIn(['header','focused']),
+        list : state.getIn(['header','list'])
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
         handleInputFocus(){
+            dispatch(actionCreators.getList());
             dispatch(actionCreators.searchFocus());
         },
         handleInputBlur(){
--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,4 +1,7 @@
 import * as actionTypes from './constants';
+import {fromJS} from 'immutable';
+import Axios from 'axios';
+import { from } from 'rxjs';
 
 export const searchFocus = () => ({
     type  : actionTypes.SEARCH_FOCUS
@@ -7,3 +10,19 @@ export const searchFocus = () => ({
 export const searchBlur = () => ({
     type  : actionTypes.SEARCH_BLUR
 });
+
+const changeList = (data) => ({
+    type : actionTypes.CHANGE_LIST,
+    data : fromJS(data)
+})
+
+export const getList = () => {
+    return (dispatch) => {
+        Axios.get('/api/headerList.json').then((res) => {
+            const data = res.data;
+            dispatch(changeList(data.data));
+        }).catch(() => {
+            console.log('error');
+        })
+    }
+}
